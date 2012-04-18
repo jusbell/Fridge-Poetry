7e9db03ee96ccfc011406b3c26ac7e04af788993
Justin B <jusbell82@gmail.com>
2012-04-18 16:32:35 -0400

    Initial Commit

---
+++ client.js
@@ -0,0 +1,268 @@
+(function(){
+
+var interval = 200,
+	update = interval * 2,
+	liveUpdate = false, // expiramental
+	broadcastLockEvent = true,
+	zIndex = 1,
+	maxZ = 1000,
+	fridge = null,
+	fridgeH = null,
+	fridgeW = null,
+	words = null,
+	message = null,
+	overlay = null,
+	httpPort = 8090,
+	wsPort = 8091,
+	httpUrl = 'http://localhost:'+httpPort,
+	wsUrl = 'http://localhost:'+wsPort,
+	socket = io.connect(wsUrl);
+
+/**
+ * Returns the current time in ms
+ * @return {Number} The current time
+ */
+function now(){
+	return new Date().getTime();
+}
+
+/**
+ * Request to lock the current word. This call is synchronous so that a client
+ * cannot start to drag before knowing if the word is locked.
+ */
+function canMove(){
+	var isLocked = $(this).hasClass('locked');
+	if (!isLocked){
+		$.ajax(httpUrl+'/lock', {
+			data:{id:this.id},
+			dataType:'json',
+			async:false,
+			success:function(data, textStatus, jqXHR){
+				isLocked = data.locked;
+				if (broadcastLockEvent){
+					socket.emit('lock', data);
+				}
+			}
+		});
+		return isLocked;
+	} else {
+		return !isLocked;
+	}
+	
+}
+
+/**
+ * Initialize the words on the fridge, and set up some event handlers
+ * @param  {Object} data The initialization data
+ */
+function init(data){
+	for(var i in data){
+		var def = data[i];
+		var tmp = $('<span>').addClass('word')
+			.prop('id', i)
+			.prop('draggable', true)
+			.css({ top:def.y, left:def.x })
+			.text(def.text);
+
+		$('#fridge').append(tmp);
+	}
+
+	$('[draggable]').each(function(){
+		var me = $(this);
+		me.data('height', me.outerHeight());
+		me.data('width', me.outerWidth());
+		me[0].style.zIndex = zIndex++;
+	})
+	.on('dragstart', DragHandler.onStart)
+	.on('drag', {relative:true}, DragHandler.onDrag)
+	.on('dragend', DragHandler.onEnd );
+}
+
+
+/**
+ * Move the words along a given path
+ * @param  {Object} data Data associated with the move event
+ */
+function moveWords(data){
+	arr = JSON.parse(data);
+	if (!$.isArray(arr)){
+		arr = [arr];
+	}
+	for(var i=0; i<arr.length; i++){
+		var el = $('#'+arr[i].id);
+
+		el.animate({crSpline: $.crSpline.buildSequence(arr[i].path)}, arr[i].duration, (function(isLocked){
+			return function(){
+				if (!isLocked && $(this).hasClass('locked')){
+					$(this).removeClass('locked');
+				}
+			};
+		})(arr[i].locked));
+
+	}
+}
+
+/**
+ * Sets words for all connected clients, a password must be provided.
+ * @param {String} sentence The words
+ * @param {pwd} pwd The password
+ */
+function setWords(sentence, pwd){
+	socket.emit('setwords', {text:sentence, width:fridgeW, height:fridgeH, pwd:pwd});
+}
+
+/**
+ * Change the words on the screen for all users. If there was an error while changing
+ * the words, the user that initiated the chagne will see the error and nothing will happen.
+ * @param  {Object} data New initialization data, or an error
+ */
+function changeWords(data){
+	var hideOverlay = function(){
+		message.fadeOut('fast', function(){
+			overlay.fadeOut('fast');
+		});
+	};
+
+	var msg = data.error ? data.error : 'Loading new words...';
+	var timeout = 1000;
+	var fn = data.error ? hideOverlay : function(){
+			$('#fridge').html('');
+			init(data);
+			hideOverlay();
+		};
+	
+
+	if (data.error){
+		$('p', message).addClass('error');
+	} else {
+		$('p', message).removeClass('error');
+	}
+
+	$('p', message).html(msg);
+	overlay.fadeIn('fast', function(){
+		message.fadeIn('fast', function(){
+			setTimeout(fn, timeout);
+		});
+	});
+}
+
+/**
+ * Represents a point on the fridge
+ * @param {Object} dd The drag/drop event object
+ */
+function Point(dd){
+	this.left = Math.min( dd.limit.right, Math.max( dd.limit.left, dd.offsetX ) );
+	this.top = Math.min( dd.limit.bottom, Math.max( dd.limit.top, dd.offsetY ) );
+}
+
+/**
+ * A container for drag and drop handler functions
+ * @type {Object}
+ */
+var DragHandler = {
+	onStart:function(e, dd){
+		var isMoveable = canMove.call(this);
+
+		var me = $(this);
+		if (!isMoveable){
+			dd.cancel = true;
+		}
+
+		me.data('oldZ', this.style.zIndex);
+		this.style.zIndex = maxZ;
+		$(this).addClass('dragging');
+
+		dd.startTime = new Date().getTime();
+		dd.intervalTime = dd.startTime;
+		dd.updateTime = dd.startTime;
+
+		dd.path = [[dd.originalX, dd.originalY]];
+
+		dd.limit= {top:0, left:0};
+		dd.limit.bottom = fridgeH - $(this).data('height')-2;
+		dd.limit.right = fridgeW - $(this).data('width')-2;
+	},
+
+	onDrag:function(e, dd){
+		if (dd.cancel) return;
+		
+		var point = new Point(dd);
+
+		this.style.top = point.top + 'px';
+		this.style.left = point.left + 'px';
+
+		if (!$.isArray(dd.path)){
+			dd.path=[];
+		}
+
+		// add a point at the configured update interval
+		var ctime = now();
+		if ((ctime - dd.intervalTime) > interval){
+			dd.intervalTime = ctime;
+			dd.path.push([point.left, point.top]);
+		}
+
+		// live update, this is an expiramental feature...
+		/*
+		if (liveUpdate && (ctime - dd.updateTime) > update){
+			console.debug('live update');
+			dd.updateTime = now();
+			socket.emit('update', {
+				id: this.id,
+				x: point.left,
+				y: point.top,
+				duration: dd.updateTime - dd.startTime,
+				path: dd.path
+			});
+
+			dd.updateTime = dd.startTime = ctime;
+			dd.path=[];
+		}
+		*/
+	},
+
+	onEnd:function(e, dd){
+		if (dd.cancel) return;
+		
+		var point = new Point(dd);
+
+		this.style.zIndex = $(this).data('oldZ');
+		$(this).removeClass('dragging');
+
+		dd.endTime = now();
+		dd.path.push([point.left, point.top]);
+
+		// Emit the unlock message which will update all connected clients
+		socket.emit('unlock', {
+			id: this.id,
+			x: point.left,
+			y: point.top,
+			duration: dd.endTime - dd.startTime,
+			path: dd.path
+		});
+	}
+};
+
+$(function(){
+	overlay = $('#overlay').hide();
+	message = $('#message').hide();
+	fridge = $('#fridge'),
+	fridgeH = fridge.outerHeight(),
+	fridgeW = fridge.outerWidth(),
+
+	socket.on('init', init);
+	socket.on('update', moveWords);
+	socket.on('setwords', changeWords);
+	socket.on('lock', function(data){
+		if (data.locked){
+			$('#'+data.id).addClass('locked');
+		}
+	});
+	
+	socket.emit('init', {width:fridgeW, height:fridgeH});
+});
+
+// expose setWords so it can be run from the javascript console
+window.setWords = setWords;
+
+})(jQuery);
---
+++ server.js
@@ -0,0 +1,283 @@
+var url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    events = require("events"),
+    logger = require('socket.io/lib/logger.js'),
+
+	log = new logger(),
+	cuid = 0,
+	timerId = null,
+	timerInterval = 200,
+	wordfile = 'words-test.json',
+	notifications = [],
+	httpPort = 8090,
+	wsPort = 8091,
+	pwd = 'changeme';
+
+// check for command line switches
+if (process.argv.length > 2){
+	for(var i=2; i<process.argv.length; i++){
+		var arg = process.argv[i];
+		if (arg.indexOf('=') > 0){
+			var args = arg.split('=');
+			switch(args[0]){
+				case 'http':
+				case 'httpPort':
+					httpPort = parseInt(args[1],10);
+					break;
+				case 'ws':
+				case 'wsPort':
+					wsPort = parseInt(args[1],10);
+					break;
+				case 'pwd':
+				case 'password':
+					pwd = args[1];
+					break;
+				case 'wordfile':
+					wordfile = args[1];
+					break;
+			}
+		}
+	}
+}
+
+fs.readFile(wordfile, function(err,data){
+	if(err) {
+		log.error("default words.json file not found: %s", err);
+		process.exit(1);
+	}
+	defaultWords = JSON.parse(data);
+});
+
+// start listening
+var io = require('socket.io').listen(wsPort);
+var app = require('http').createServer(httpHandler).listen(httpPort);
+
+log.info('Listening for HTTP requests on http://localhost:'+httpPort);
+log.info('Listening for WS requests on http://localhost:'+wsPort);
+
+
+
+/**
+ * Object which represents a word
+ * @param {text} text The text of the word
+ * @param {int} x The starting x point
+ * @param {int} y The starting y point
+ */
+function Word(text, x, y){
+	this.text = text;
+	this.x = x || 0;
+	this.y = y || 0;
+	this.locked = false;
+	this.id = 'w' + uid();
+}
+
+function shuffle(arr){
+    var result = [], rand;
+    arr.forEach(function(value, i) {
+		rand = Math.floor(Math.random() * (i + 1));
+		result[i] = result[rand];
+		result[rand] = value;
+    });
+    return result;
+}
+
+/**
+ * Generates a unique id
+ * @return {int} The unique id
+ */
+function uid(){
+	return cuid++;
+}
+
+/**
+ * Gets the current state of the fridge
+ * @param  {int} width	The width of the fridge
+ * @param  {int} height The height of the fridge
+ * @return {object}		The words
+ */
+function getCurrentState(width, height){
+	if (!global.words){
+		global.words = {};
+		initBoard(defaultWords, width, height);
+	}
+	return global.words;
+}
+
+/**
+ * Initializes the board and sets up the initial position (randomly) of all words
+ * @param  {String|Array} str An array of words, or a space delimited string of words
+ * @param  {int} width The fridge width
+ * @param  {int} height The fridge height
+ */
+function initBoard(words, width, height){
+	log.info('Init fridge (w:'+width+', h:'+height+') with word string: ' + words);
+	if (!Array.isArray(words)){
+		words = words.split(' ');
+	}
+	words.forEach(function(text){
+		var x = Math.abs( (Math.floor(Math.random() * width) + 1) - 25);
+		var y = Math.abs( (Math.floor(Math.random() * height) + 1) -25);
+		var tmp = new Word(text, x, y);
+		global.words[tmp.id] = tmp;
+	});
+}
+
+/**
+ * Serves a static file HTML/CSS/JS based on the given uri
+ * @param  {string} uri The current request URI
+ * @param  {ServerResponse} resp The HTTP server response object
+ */
+function serveStaticFile(uri, resp) {
+	var	filename = path.join(process.cwd(), (uri == '/') ? '/index.html' : uri),
+		extension = path.extname(filename),
+		contentType = null;
+
+	switch(extension){
+		case '.js': contentType = 'text/javascript'; break;
+		case '.css': contentType = 'text/css'; break;
+		default: contentType = 'text/html'; break;
+	}
+
+	path.exists(filename, function(exists) {
+		if(exists) {
+			fs.readFile(filename, function(err, content) {
+				if(err) {
+					resp.writeHead(500);
+					resp.end();
+				} else {
+					resp.writeHead(200, { 'Content-Type': contentType });
+					resp.end(content, 'utf-8');
+				}
+			});
+		} else {
+			resp.writeHead(404, {"Content-Type": contentType});
+			resp.write("404 Not Found\n");
+			resp.end();
+		}
+	});
+}
+
+/**
+ * The core http server function to handle incoming requests
+ * @param  {ServerRequest} request The server request
+ * @param  {ServerResponse} resp The server response
+ */
+function httpHandler(req, resp){
+	var reqData = url.parse(req.url, true),
+		uri = reqData.pathname,
+		id = null,
+		tmp = null,
+		now = new Date();
+
+	resp.json = function(data){
+		var json = JSON.stringify(data);
+		this.writeHead(200, {'Content-Type':'application/json'});
+		this.write(json);
+		this.end();
+	};
+
+	log.info('Http request start-  ' + now.toJSON());
+	log.debug(reqData);
+
+	switch(uri){
+		case '/init':
+			resp.json(getCurrentState());
+			break;
+
+		case '/lock':
+			id = reqData.query.id;
+			if (!global.words[id].locked){
+				global.words[id].locked = true;
+			}
+			resp.json({id:id, locked:global.words[id].locked});
+			break;
+
+		default:
+			serveStaticFile(uri, resp);
+			break;
+	}
+}
+
+/**
+ * Set up the socket io functions on connection
+ * @param  {socket} socket The web socket
+ */
+io.sockets.on('connection', function (socket) {
+
+	/**
+	 * Updates clients with a words current position
+	 * @param  {Object} data The data
+	 */
+	var update = function(data){
+		global.words[data.id].x = data.x;
+		global.words[data.id].y = data.y;
+		socket.broadcast.emit('update', JSON.stringify({
+				id:data.id,
+				path:data.path,
+				duration:data.duration,
+				locked:global.words[data.id].locked
+			})
+		);
+	};
+
+	/**
+	 * Unlock a word so other users can drag it, also update it's position
+	 * and broadcast the position to all other users
+	 * @param  {object} data The socket request data
+	 */
+	socket.on('unlock', function(data){
+		log.info('WS:/unlock', data);
+		global.words[data.id].locked = false;
+		update(data);
+		
+	});
+
+	/**
+	 * Emits a lock event to all listeners
+	 * @param  {object} data The socket request data
+	 */
+	socket.on('lock', function(data){
+		log.info('WS:/lock', data);
+		socket.broadcast.emit('lock', data);
+	});
+
+	/**
+	 * Triggers an update event to clients
+	 * @param  {Object} data The WS request data
+	 */
+	socket.on('update', function(data){
+		log.info('WS:/update', data);
+		update(data);
+	});
+
+	/**
+	 * Initialize the board for the client, new client connections will be given
+	 * the current state of the board
+	 * @param  {object} data The socket request data
+	 */
+	socket.on('init', function(data){
+		log.info('WS:/init');
+		socket.emit('init', getCurrentState(data.width, data.height));
+	});
+
+	/**
+	 * Changes the words on the board, and updates all client boards
+	 * @param  {object} data The socket request data
+	 */
+	socket.on('setwords', function(data){
+		log.info('WS:/setwords', data);
+		if (data.pwd == pwd){
+			global.words = null;
+			defaultWords = data.text;
+			io.sockets.emit('setwords', getCurrentState(data.width, data.height));
+		} else if (!data.pwd){
+			log.warn('Cannot change words, no password provided');
+			socket.emit('setwords', {error:'You must provide a password to change the words'});
+		} else {
+			log.warn('Client password does not match server password');
+			socket.emit('setwords', {error:'Password does not match, please try again'});
+		}
+	});
+
+});
